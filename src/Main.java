65261d8f58744d398f2650cac611a9e4d602db3b
Ivan <volkivanv@mail.ru>
2024-02-20 00:53:09 +0300

    Сделано 7 заданиеÐ

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
+import core.calculator.Calculator;
+import core.calculator.ComlexNumber;
+import core.logger.Logger;
+import core.logger.impl.LoggerTxt;
+import core.view.View;
+
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Logger logger = new LoggerTxt("log.txt");
+        View view = new View(logger);
+        view.run();
+
     }
 }
